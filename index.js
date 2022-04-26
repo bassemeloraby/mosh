d53b35bd3664739495e594acbd59937bf9896e24
bassem eloraby <BASEM12683@GMAIL.COM>
2022-04-26 14:27:32 +0300

    11- Input Validation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 const courses = [
   { id: 1, name: 'course1' },
   { id: 2, name: 'course2' },
@@ -17,6 +18,15 @@ app.get('/api/courses', (req, res) => {
 
 //create course
 app.post('/api/courses', (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
   const course = {
     id: courses.length + 1,
     name: req.body.name,
