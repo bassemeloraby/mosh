9c263d7287f21f659aef45e52fd730f9c63a326a
bassem eloraby <BASEM12683@GMAIL.COM>
2022-04-27 11:04:25 +0300

    express-static-public

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(logger);
+
+// app.use(function (req, res, next) {
+//   console.log('authenticating....');
+//   next();
+// });
+
 const courses = [
   { id: 1, name: 'course1' },
   { id: 2, name: 'course2' },
